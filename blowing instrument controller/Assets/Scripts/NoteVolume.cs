2cfcc47532dac861047f2bb1bfcc36113fa988f8
Rebecca_YUAN <ziyumoyan@126.com>
2021-05-27 00:16:11 +0800

    unity

---
+++ blowing instrument controller/Assets/Scripts/NoteVolume.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteVolume : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pressure = SocketClient.pressure;
+        Debug.Log(pressure);
+    }
+
+    public void setVolume()
+    {
+        this.GetComponent<AudioSource>().volume = 1;
+    }
+}
